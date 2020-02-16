cd7e27a8e263768ec0d3e2091fb1d404472aa079
William Werst <werstiv@gmail.com>
2020-02-16 12:34:37 -0800

    Add more test vectors to kinematics

--- hockbot/tests/test_scara_kinematics.py
+++ hockbot/tests/test_scara_kinematics.py
@@ -10,30 +10,29 @@ from hockbot.scara_kinematics import (
 
 
 class TestScaraKinematics(unittest.TestCase):
+    TEST_VECS = [
+        (np.array([0      ,        0]), np.array([ 1.0, 0.0])),
+        (np.array([np.pi/2,        0]), np.array([ 0.5, 0.5])),
+        (np.array([np.pi/2, -np.pi/2]), np.array([ 0.0, 1.0])),
+        (np.array([np.pi/2,  np.pi/2]), np.array([ 0.0, 0.0])),
+        (np.array([np.pi  , -np.pi/2]), np.array([-0.5, 0.5])),
+        (np.array([np.pi/4,        0]), np.array([ 0.5+np.sqrt(0.125), np.sqrt(0.125)])),
+        (np.array([np.pi/4, -np.pi/4]), np.array([ 0.5*np.sqrt(2), 0.5*np.sqrt(2)])),
+    ]
 
     def test_fkin(self):
-        test_vecs = [
-            (np.array([0      ,        0]), np.array([ 1.0, 0.0])),
-            (np.array([np.pi/2,        0]), np.array([ 0.5, 0.5])),
-            (np.array([np.pi/2, -np.pi/2]), np.array([ 0.0, 1.0])),
-            (np.array([np.pi/2,  np.pi/2]), np.array([ 0.0, 0.0])),
-            (np.array([np.pi  , -np.pi/2]), np.array([-0.5, 0.5])),
-        ]
-        for joints, expected_pos in test_vecs:
+        # Test fkin
+        for joints, expected_pos in self.TEST_VECS:
             act_pos = fkin(*joints)
             assert np.allclose(
                 act_pos,
                 expected_pos)
 
     def test_ikin_of_fkin(self):
-        test_vecs = [
-            (np.array([0      ,        0]), np.array([ 1.0, 0.0])),
-            (np.array([np.pi/2,        0]), np.array([ 0.5, 0.5])),
-            (np.array([np.pi/2, -np.pi/2]), np.array([ 0.0, 1.0])),
-            (np.array([np.pi/2,  np.pi/2]), np.array([ 0.0, 0.0])),
-            (np.array([np.pi  , -np.pi/2]), np.array([-0.5, 0.5])),
-        ]
-        for joints, expected_pos in test_vecs:
+        # Assumes that fkin is trustworthy, and then
+        # uses trustworthy fkin to verify output of
+        # ikin is correct
+        for joints, expected_pos in self.TEST_VECS:
             act_pos = fkin(*joints)
             ikin_joints = ikin(*act_pos)
             for possible_joints in ikin_joints:
