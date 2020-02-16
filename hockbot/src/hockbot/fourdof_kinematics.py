138f0c0c03f68bbb4acd386b9426619de51a256f
dkornfeld <davidedikornfeld@gmail.com>
2020-02-15 17:11:20 -0800

    finished surface play ikin

--- hockbot/src/hockbot/fourdof_kinematics.py
+++ hockbot/src/hockbot/fourdof_kinematics.py
@@ -12,7 +12,7 @@ import rospkg
 
 URDF_PATH = rospkg.RosPack().get_path('hockbot') + '/urdf/robot_4dof.urdf'
 robot = URDF.from_xml_file(URDF_PATH)
-joints = robots.joints
+joints = robot.joints
 
 # Constants ----------------------------------------------------------------------------------------
 
@@ -47,46 +47,37 @@ def Rz(q):
 
 def fkin(q0, q1, q2, q3, q4):
     '''
-    Returns (x, y, z, theta, phi, grip) as a function of actuator positions.
+    Returns (x, y, z, theta, grip) as a function of actuator positions.
     q0-q3 take values in [0, 2Pi)
     q4 takes values 0 or 1 (electromagnet off or on)
 
-    phi represents yaw of the gripper around the world z axis
-    theta represents pitch of gripper WRT the horizon (world xy plane)
+    theta represents pitch of gripper WRT vertical
     '''
-    
-    joint_thetas = [q0, q1, q2, q3, q4]
-    i = 0
 
-    # start before Joint 0
-    x = vec(0,0,0)
-    R = Rz(0)
+    # Radial distance from base
+    R = L1*np.sin(q1) + L2*np.sin(q1 + q2) + TIP_LENGTH*np.sin(q1 + q2 + q3)
 
-    for joint in joints:
-        # If joint has an origin field, it's just a translation and maybe rotation
-        if joint.origin:
-            xyz = joint.origin.xyz
-            rpy = joint.origin.rpy
-            x = x + R.apply(vec(xyz[0], xyz[1], xyz[2]))
-            R = R * Rx(rpy[0]) * Ry(rpy[1]) * Rz(rpy[2])
-            
-        # if joint has an axis field, it's just a rotation about a motor
-        if joint.axis:
-            xyz = joint.axis.xyz
-            R = R * 
-                Rx(joint_thetas[i] * xyz[0]) * 
-                Ry(joint_thetas[i] * xyz[1]) * 
-                Rz(joint_thetas[i] * xyz[2])
-            i = i + 1
+    # and height above robot z
+    z = L1*np.cos(q1) + L2*np.cos(q1 + q2) + TIP_LENGTH*np.cos(q1 + q2 + q3)
 
-    # Extract the tip position from the R matrix
-    angles = R.as_euler('xyz')
-    phi = angles[2] # Phi is just the z axis rotation
-    theta = angles[1] # Theta is just the y axis rotation
+    # x and y displacements from robot origin
+    x = R*np.cos(q0)
+    y = R*np.sin(q0)
 
-    return (x[0], x[1], x[2], theta, phi, grip)
+    # Adjust for world coordinates
+    x = x + X_OFFSET
+    y = y + Y_OFFSET
+    z = z + Z_OFFSET
 
-def ikin(x, y, z, surface):
+    # Calculate theta
+    theta = q1 + q2 + q3
+
+    # Grip is just q4
+    grip = q4
+
+    return (x, y, z, theta, grip)
+
+def ikin(x, y, z, surface=SURFACE_PLAY):
     '''
     Returns (q0, q1, q2, q3) as a function of requested cartesian position.
     If surface = SURFACE_PLAY then (x, y) refers to a position in the playing area relative to
@@ -106,10 +97,13 @@ def ikin(x, y, z, surface):
     # Check out of bounds/reach
     if (surface == SURFACE_PLAY):
         if (x**2 + y**2 + (z + TIP_LENGTH)**2 > (L1+L2)**2): # Cannot reach longer than arms
+            print('reach error')
             return None
-        elif (y <= 0): # Cannot reach behind itself in play mode
+        elif (y < 0): # Cannot reach behind itself in play mode
+            print('Cant go behind')
             return None
         elif (z < TIP_LENGTH - Z_OFFSET): # Don't crash into the table
+            print('crash')
             return None
 
     if (surface == SURFACE_COLLECT):
@@ -124,12 +118,12 @@ def ikin(x, y, z, surface):
         # Calculate the 3 joints above the base joint
 
         R = np.sqrt(x**2 + y**2) # Distance in the cartesian plane from base to tip touchdown
-        r = np.sqrt(R**2 + z**2) # Distance from q1 to q3 joints
+        r = np.sqrt(R**2 + (z + TIP_LENGTH)**2) # Distance from q1 to q3 joints
 
         q2 = np.arccos((r**2 - L1**2 - L2**2)/(2*L1*L2)) # Law of cosines solves q2 and q1
 
         # Difference of angle to pointer and interior angle
-        q1 = np.arctan2(R/z) - np.arctan2((L2*np.sin(q2))/(L1 + L2*np.cos(q2)))
+        q1 = np.arctan2(R,(z + TIP_LENGTH)) - np.arctan2((L2*np.sin(q2)),(L1 + L2*np.cos(q2)))
 
         q3 = np.pi - q2 - q1 # Net 180 degree rotation
 
@@ -137,3 +131,13 @@ def ikin(x, y, z, surface):
         q0 = np.pi/2.0 # Fix the base joint to point directly forward
 
     return (q0, q1, q2, q3)
+
+
+if __name__ == "__main__":
+    theta0 = 3
+    theta1 = 1.13
+    theta2 = 0.73
+    theta3 = np.pi - theta1 - theta2
+    a = fkin(theta0, theta1, theta2, theta3, 0)
+    print(a)
+    print(ikin(a[0], a[1], a[2]))
