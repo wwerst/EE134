6740e932de562ec2826eab8ebb1cd112a38df8fb
David Kornfeld <davidedikornfeld@gmail.com>
2020-03-04 22:12:05 -0800

    first pass on puck predicter node

--- hockbot/src/hockbot/puck_dynamics.py
+++ hockbot/src/hockbot/puck_dynamics.py
@@ -5,6 +5,7 @@
 import numpy as np
 import rospy
 from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import Point
 
 class PuckDynamics(object):
     '''
@@ -12,19 +13,24 @@ class PuckDynamics(object):
     '''
     def __init__(self):
         '''
+        Initialize the puck dynamics object with constants for the environment
         '''
         self.NUM_POINTS = 10 # Number of points to fit
         self.TABLE_LEFT = 0.0 # x Position of table left edge, meters
-        self.TABLE_RIGHT = 0.5 # x Position of the table right edge, meters
+        self.TABLE_RIGHT = 10.0 # x Position of the table right edge, meters
         self.TABLE_BOTTOM = 0.0 # y Position of table bottom edge, meters
-        self.TABLE_TOP = 2.0 # y Position of table top edge, meters
-        self.PUCK_RADIUS = 0.04 # Puck radius, meters
-        self.ELASTICITY = 0.6 # Elasticity coefficient, unitless
+        self.TABLE_TOP = 10.0 # y Position of table top edge, meters
+        self.PUCK_RADIUS = 1 # Puck radius, meters
+        self.ELASTICITY = 0.5 # Elasticity coefficient, unitless
 
         # Storage for timestamps and x/y positions of puck
         self.t_arr = np.zeros(self.NUM_POINTS)
         self.x_arr = np.zeros(self.NUM_POINTS)
         self.y_arr = np.zeros(self.NUM_POINTS)
+
+        # Present position and velocity of puck since last update
+        self.position = np.zeros(2)
+        self.velocity = np.zeros(2)
 
     def add_point(stamped_point):
         '''
@@ -50,12 +56,28 @@ class PuckDynamics(object):
         self.x_coeffs = np.polyfit(self.t_arr, self.x_arr, 2)
         self.y_coeffs = np.polyfit(self.t_arr, self.y_arr, 2)
 
+        # Fill in present position and velocity
+        self.position = np.array([x, y])
+        vx = 0.5 * (
+                ((self.x_arr[NUM_POINTS-1] - self.x_arr[NUM_POINTS-2]) / 
+                (self.t_arr[NUM_POINTS-1] - self.t_arr[NUM_POINTS-2])) + 
+                ((self.x_arr[NUM_POINTS-2] - self.x_arr[NUM_POINTS-3]) / 
+                (self.t_arr[NUM_POINTS-2] - self.t_arr[NUM_POINTS-3])) + 
+                )
+        vy = 0.5 * (
+                ((self.y_arr[NUM_POINTS-1] - self.y_arr[NUM_POINTS-2]) / 
+                (self.t_arr[NUM_POINTS-1] - self.t_arr[NUM_POINTS-2])) + 
+                ((self.y_arr[NUM_POINTS-2] - self.y_arr[NUM_POINTS-3]) / 
+                (self.t_arr[NUM_POINTS-2] - self.t_arr[NUM_POINTS-3])) + 
+                )
+        self.velocity = np.array([vx, vy])
+
     def predict_position(t):
         '''
         Predicts the position of the puck as a function of the requested robot time
         '''
 
-        # Account for no reverse direction. Assumes vertex is always in future
+        # Account for no reverse direction due to slowdown. Assumes vertex is always in future
         if t >= (-1.0 * self.x_coeffs[1])/(2.0 * self.x_coeffs[0]):
             x = np.polyval(self.x_coeffs, (-1.0 * self.x_coeffs[1])/(2.0 * self.x_coeffs[0]))
         else:
@@ -68,13 +90,20 @@ class PuckDynamics(object):
 
         # Account for hitting walls, reflect with some elasticity coefficient
         if x < self.TABLE_LEFT + self.PUCK_RADIUS:
-            x = self.TABLE_LEFT + ((self.TABLE_LEFT - (x - self.PUCK_RADIUS)) * self.ELASTICITY) + self.PUCK_RADIUS
+            x = self.TABLE_LEFT + ((self.TABLE_LEFT - (x - self.PUCK_RADIUS)) * self.ELASTICITY) 
+                + self.PUCK_RADIUS
         elif x > self.TABLE_RIGHT - self.PUCK_RADIUS:
-            x = self.TABLE_RIGHT - ((x + self.PUCK_RADIUS - self.TABLE_RIGHT) * self.ELASTICITY) - self.PUCK_RADIUS
+            x = self.TABLE_RIGHT - ((x + self.PUCK_RADIUS - self.TABLE_RIGHT) * self.ELASTICITY) 
+                - self.PUCK_RADIUS
 
         if y < self.TABLE_BOTTOM + self.PUCK_RADIUS:
-            y = self.TABLE_BOTTOM + ((self.TABLE_BOTTOM - (y - self.PUCK_RADIUS)) * self.ELASTICITY) + self.PUCK_RADIUS
+            y = self.TABLE_BOTTOM + ((self.TABLE_BOTTOM - (y - self.PUCK_RADIUS)) * self.ELASTICITY) 
+                + self.PUCK_RADIUS
         elif y > self.TABLE_TOP - self.PUCK_RADIUS:
-            y = self.TABLE_TOP - ((y + self.PUCK_RADIUS - self.TABLE_TOP) * self.ELASTICITY) - self.PUCK_RADIUS
+            y = self.TABLE_TOP - ((y + self.PUCK_RADIUS - self.TABLE_TOP) * self.ELASTICITY) 
+                - self.PUCK_RADIUS
 
-        return np.array([x, y])
+        retPoint = Point()
+        retPoint.x = x
+        retPoint.y = y
+        return retPoint
